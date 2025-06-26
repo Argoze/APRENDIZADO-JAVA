9be9083635b07bb129487e24b7a8ef86e1d00d19
Argoze <gustavoargozelopes@gmail.com>
2025-06-26 14:47:07 -0300

    primeiro exercício

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TO DO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner (System.in);
+
+        // TO DO: Exibir as mensagens para o nosso usuario
+        System.out.println("Digite o numero da conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o numedo da agencia:");
+        String agencia =  scanner.nextLine();
+
+        System.out.println("Digite o nome:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        // TO DO: Exibir a mensagem da conta criada
+        System.out.println("\nOlá " + nome + 
+        " obrigado por criar uma conta em nosso banco, sua agência: " + agencia + 
+        " sua conta: " + numero + 
+        " e seu saldo: " + saldo + " já está disponível para saque");
     }
 }
